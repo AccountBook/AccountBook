bb46f77dc40a207446a8348c949eced58435903b
Jack <369498884@qq.com>
2017-06-11 21:53:57 +0800

    empty project

---
+++ Project/MyAccountBook/MyAccountBook/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAccountBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
